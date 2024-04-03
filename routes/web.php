6ca38b042d8b0b283fca84a6fed62d33b790f33c
Gusti Alifiraqsha <alifiraqshaakbar@gmail.com>
2024-04-03 17:35:14 +0700

    modify file routes/web

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,65 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hofi', function (){
+    return "Hello Gusti Alifiraqsha Akbar👋";
+});
+
+Route::redirect('/instagram','/hofi');
+
+Route::fallback(function (){
+    return "error 404 by HOFi";
+});
+
+Route::view('/hello', 'hello', ['name' => 'Gusti']);
+
+Route::get('/hello-again', function (){
+    return view('hello', ['name' => 'Gusti']);
+});
+
+Route::get('/hello-world', function (){
+    return view('hello.world', ['name' => 'Gusti']);
+});
+
+Route::get('/products/{id}', function ($productId){
+    return "Product $productId";
+})->name('product.detail');
+
+Route::get('/products/{product}/items/{item}', function ($productId, $itemId){
+    return "Product $productId, Item $itemId";
+})->name('product.item.detail');
+
+Route::get('/categories/{id}', function ($categoryId){
+    return "Category $categoryId";
+})->where('id', '[0-9]+')->name('category.detail');
+
+Route::get('/users/{id?}', function ($userId = '404'){
+    return "User $userId";
+})->name('user.detail');
+
+Route::get('/conflict/gusti', function (){
+    return "Conflict Gusti Alifiraqsha Akbar";
+});
+
+Route::get('/conflict/{name}', function ($name){
+    return "Conflict $name";
+});
+
+Route::get('/produk/{id}', function ($id){
+    $link = route('product.detail', ['id' => $id]);
+    return "Link $link";
+});
+
+Route::get('/produk-redirect/{id}', function ($id){
+    return redirect()->route('product.detail', ['id' => $id]);
+});
+
+Route::get('/controller/hello/request', [\App\Http\Controllers\HelloController::class, 'request']);
+Route::get('/controller/hello/{name}', [\App\Http\Controllers\HelloController::class, 'hello']);
+
+Route::get('/input/hello', [\App\Http\Controllers\InputController::class, 'hello']);
+Route::post('/input/hello', [\App\Http\Controllers\HelloController::class, 'hello']);
+Route::post('/input/hello/first', [\App\Http\Controllers\HelloController::class, 'helloFirstName']);
+Route::post('/input/hello/input', [\App\Http\Controllers\HelloController::class, 'helloInput']);
+Route::post('/input/hello/array', [\App\Http\Controllers\HelloController::class, 'helloArray']);
