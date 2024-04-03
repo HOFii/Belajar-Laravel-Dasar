e7fdd0d38eaeb2bf8f962dbf1e8dd9916400b13e
Gusti Alifiraqsha <alifiraqshaakbar@gmail.com>
2024-04-03 17:30:19 +0700

    modify config folders

---
+++ config/contoh.php
@@ -0,0 +1,10 @@
+<?php
+
+return[
+    "author" => [
+        "first" => env('NAME_FIRST', 'Gusti'),
+        "last" => env('NAME_LAST', 'Akabr')
+    ],
+    "email" => "echo.alifiraqsha@gmail.com",
+    "web" => "https://github.com/"
+];
