8d15c9e536b278d389cc6e5786a10991e62e9217
Gusti Alifiraqsha <alifiraqshaakbar@gmail.com>
2024-04-04 15:17:44 +0700

    modify routes/web file

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -74,7 +75,97 @@ Route::get('/controller/hello/request', [\App\Http\Controllers\HelloController::
 Route::get('/controller/hello/{name}', [\App\Http\Controllers\HelloController::class, 'hello']);
 
 Route::get('/input/hello', [\App\Http\Controllers\InputController::class, 'hello']);
-Route::post('/input/hello', [\App\Http\Controllers\HelloController::class, 'hello']);
-Route::post('/input/hello/first', [\App\Http\Controllers\HelloController::class, 'helloFirstName']);
-Route::post('/input/hello/input', [\App\Http\Controllers\HelloController::class, 'helloInput']);
-Route::post('/input/hello/array', [\App\Http\Controllers\HelloController::class, 'helloArray']);
+Route::post('/input/hello', [\App\Http\Controllers\InputController::class, 'hello']);
+Route::post('/input/hello/first', [\App\Http\Controllers\InputController::class, 'helloFirstName']);
+Route::post('/input/hello/input', [\App\Http\Controllers\InputController::class, 'helloInput']);
+Route::post('/input/hello/array', [\App\Http\Controllers\InputController::class, 'helloArray']);
+Route::post('/input/type', [\App\Http\Controllers\InputController::class, 'inputType']);
+Route::post('/input/filter/only', [\App\Http\Controllers\InputController::class, 'filterOnly']);
+Route::post('/input/filter/except', [\App\Http\Controllers\InputController::class, 'filterExcept']);
+Route::post('/input/filter/merge', [\App\Http\Controllers\InputController::class, 'filterMerge']);
+
+Route::post('/file/upload', [\App\Http\Controllers\FileController::class, 'upload'])
+    ->withoutMiddleware([\App\Http\Middleware\VerifyCsrfToken::class]);
+
+Route::get('/response/hello', [\App\Http\Controllers\ResponseController::class, 'response']);
+Route::get('/response/header', [\App\Http\Controllers\ResponseController::class, 'header']);
+
+Route::prefix("/response/type")->group(function (){
+    Route::get('/view', [\App\Http\Controllers\ResponseController::class, 'responseView']);
+    Route::get('/json', [\App\Http\Controllers\ResponseController::class, 'responseJson']);
+    Route::get('/file', [\App\Http\Controllers\ResponseController::class, 'responseFile']);
+    Route::get('/download', [\App\Http\Controllers\ResponseController::class, 'responseDownload']);
+});
+
+Route::get('/response/type/view', [\App\Http\Controllers\ResponseController::class, 'responseView']);
+Route::get('/response/type/json', [\App\Http\Controllers\ResponseController::class, 'responseJson']);
+Route::get('/response/type/file', [\App\Http\Controllers\ResponseController::class, 'responseFile']);
+Route::get('/response/type/download', [\App\Http\Controllers\ResponseController::class, 'responseDownload']);
+
+Route::controller(\App\Http\Controllers\CookieController::class)->group(function (){
+    Route::get('/cookie/set', 'createCookie');
+    Route::get('/cookie/get', 'getCookie');
+    Route::get('/cookie/clear', 'clearCookie');
+});
+
+Route::get('/redirect/from', [\App\Http\Controllers\RedirectController::class, 'redirectFrom']);
+Route::get('/redirect/to', [\App\Http\Controllers\RedirectController::class, 'redirectTo']);
+Route::get('/redirect/name', [\App\Http\Controllers\RedirectController::class, 'redirectName']);
+Route::get('/redirect/name/{name}', [\App\Http\Controllers\RedirectController::class, 'redirectHello'])
+    ->name('redirect-hello');
+Route::get('/redirect/named', function (){
+        //    return route('redirect-hello', ['name' => 'Gusti']);
+        //    return url()->route('redirect-hello', ['name' => 'Gusti']);
+            return \Illuminate\Support\Facades\URL::route('redirect-hello', ['name' => 'Gusti']);
+});
+Route::get('/redirect/action',[\App\Http\Controllers\RedirectController::class, 'redirectAction']);
+Route::get('/redirect/away',[\App\Http\Controllers\RedirectController::class, 'redirectAway']);
+
+Route::middleware(['contoh:HOFi,401'])->prefix('/middleware')->group(function (){
+    Route::get('/api', function (){
+        return "OK";
+    });
+    Route::get('/group', function (){
+        return "GROUP";
+    });
+});
+
+Route::get('/url/action', function (){
+    // return action([\App\Http\Controllers\FormController::class, 'form'], []);
+    // return url()->action([\App\Http\Controllers\FormController::class, 'form'], []);
+    return \Illuminate\Support\Facades\URL::action([\App\Http\Controllers\FormController::class, 'form'], []);
+});
+Route::get('/form', [\App\Http\Controllers\FormController::class, 'form']);
+Route::post('/form', [\App\Http\Controllers\FormController::class, 'submitForm']);
+
+Route::get('/url/current', function (){
+    return \Illuminate\Support\Facades\URL::full();
+});
+
+Route::get('/session/create', [\App\Http\Controllers\SessionController::class, 'createSession']);
+Route::get('/session/get', [\App\Http\Controllers\SessionController::class, 'getSession']);
+
+Route::get('/error/sample', function (){
+    throw new Exception("Sample Error");
+});
+
+Route::get('/error/manual', function (){
+    report(new Exception("Sample Error"));
+    return "OK";
+});
+
+Route::get('/error/validation', function (){
+    throw new \App\Exceptions\ValidationException("Validation Error");
+});
+
+Route::get('/abort/400', function (){
+    abort(400, "Ups Validation Error");
+});
+
+Route::get('/abort/401', function (){
+    abort(401);
+});
+
+Route::get('/abort/500', function (){
+    abort(500);
+});
